7e263391b76f3439a39f8ce9bdeaec8de8f69e30
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-16 12:52:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/referrals", require("./routes/referrals"));
 app.use("/api/admin", require("./routes/admin"));
+app.use("/user", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
